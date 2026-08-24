d7400bfef44f6dde3aeb9f7def9d11fdd512413e
Blooperly <jbrownfisher@gmail.com>
2026-08-23 23:15:16 -0700

    More alignment fixes

--- Chasm_Inspector.js
+++ Chasm_Inspector.js
@@ -310,7 +310,7 @@ function showInspector(id) {
 		case iid.currency_bugs:
 			setInspectorTitle(chasm_currency[cid.currency_bugs].inspector_symbol + "Velocity Beetles", redraw_inspector);
 			setInspectorCost("", redraw_inspector);
-			setInspectorText("Okay this one sucks. When you drop agile creatures into the Chasm tiny silver-blue beetles crawl out at a blinding speed. Your researchers have managed to catch a few, and say they can be ground into a paste and converted directly into physical momentum. They remind you of horrible, multi-legged video game hedgehogs.", redraw_inspector);
+			setInspectorText("Okay, this one sucks. When you drop agile creatures into the Chasm tiny silver-blue beetles crawl out at a blinding speed. Your researchers have managed to catch a few, and say they can be ground into a paste and converted directly into physical momentum. They remind you of horrible, multi-legged video game hedgehogs.", redraw_inspector);
 			setInspectorDivider(false, redraw_inspector);
 			setInspectorSubtext("", redraw_inspector);
 			break;
